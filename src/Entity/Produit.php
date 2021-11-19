c4fa75bbb863eff2ffdfe0762ec311432be47362
Moussa Ba <moussa10ba@gmail.com>
2021-11-19 00:27:17 +0000

    produit categorie commit

---
+++ src/Entity/Produit.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=ProduitRepository::class)
+ */
+class Produit
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $paysDorigine;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prixKg;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $quantite;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Categorie::class, inversedBy="produits")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPaysDorigine(): ?string
+    {
+        return $this->paysDorigine;
+    }
+
+    public function setPaysDorigine(string $paysDorigine): self
+    {
+        $this->paysDorigine = $paysDorigine;
+
+        return $this;
+    }
+
+    public function getPrixKg(): ?float
+    {
+        return $this->prixKg;
+    }
+
+    public function setPrixKg(float $prixKg): self
+    {
+        $this->prixKg = $prixKg;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?float
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(float $quantite): self
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+}
